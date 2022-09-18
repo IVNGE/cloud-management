c6cd96c49b51bcec3d6aa90f468ee9a20e67f6f8
Iván González <Ivan.g.e.017@gmail.com>
2022-09-17 23:25:21 -0600

    Add about, All-in-one, Support, and pricing components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero"
+import About from "./Components/About";
+import Support from "./Components/Support";
+import AllInOne from "./Components/AllInOne";
+import Pricing from "./Components/Pricing";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
+      <Support />
+      <AllInOne />
+      <Pricing />
     </>
   );
 }
